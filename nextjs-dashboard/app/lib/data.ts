10729d8695e8486aba2444f64165a9d4c9c62fa1
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-11-27 17:08:45 +0900

    Feat: handle error

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -158,13 +158,13 @@ export async function fetchInvoiceById(id: string) {
 
   try {
     const data = await sql<InvoiceForm>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
+		SELECT
+		invoices.id,
+		invoices.customer_id,
+		invoices.amount,
+		invoices.status
+		FROM invoices
+		WHERE invoices.id = ${id};
     `;
 
     const invoice = data.rows.map((invoice) => ({
@@ -172,6 +172,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+
+    console.log(invoice);
 
     return invoice[0];
   } catch (error) {
